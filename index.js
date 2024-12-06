3c4dbf6d41f305657a43c6969f9d3a7e9834a77b
Fatema Tabassum Diba <fatematabassum00700@gmail.com>
2024-12-06 17:18:20 +0600

    All movies put,get,delete operation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
@@ -24,6 +24,39 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // add movie form
+    const movieDb = client.db("MovieDB");
+    const movieCollection = movieDb.collection("movieCollection");
+    const userCollection = movieDb.collection("userCollection");
+
+    app.post("/add-movie", async (req, res) => {
+      const formData = req.body;
+      console.log(formData);
+      const result = await movieCollection.insertOne(formData);
+      res.send(result);
+    });
+
+    app.get("/all-movies", async (req, res) => {
+      const movies = movieCollection.find();
+      const result = await movies.toArray();
+      res.send(result);
+    });
+    // details
+    app.get("/all-movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/all-movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
